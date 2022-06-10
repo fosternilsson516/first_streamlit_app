8360e3cb4b7ea5cb10c3bd2065dd1666f9be85d9
fosternilsson516 <99349623+fosternilsson516@users.noreply.github.com>
2022-06-10 16:01:30 -0500

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("hello world")
+streamlit.text('🥣omega 3 & blueberry oatmeal')
+streamlit.text('🥬kale spinnach & rocket smoothie')
+streamlit.text('🍳hardboiled free range eggs')
+streamlit.text('🥑avocado toast')

ea007e32c77aa74fb3b5451827d636af52389281
fosternilsson516 <99349623+fosternilsson516@users.noreply.github.com>
2022-06-11 01:36:30 -0500

    Update streamlit_app.py
    added pandas and created data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text('🍳hardboiled free range eggs')
 streamlit.text('🥑avocado toast')
 streamlit.header('🍓🫐build your own fruit smoothie🍌🍒')
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)

8789b71041f408198fd7f42443591b103a91441a
fosternilsson516 <99349623+fosternilsson516@users.noreply.github.com>
2022-06-11 01:45:44 -0500

    Update streamlit_app.py
    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,8 @@ streamlit.text('🥑avocado toast')
 streamlit.header('🍓🫐build your own fruit smoothie🍌🍒')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("pick some fruits:" , list(my_fruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
+

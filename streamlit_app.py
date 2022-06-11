dff13f768161217970778fd18d02ce05e1aef0af
fosternilsson516 <99349623+fosternilsson516@users.noreply.github.com>
2022-06-11 02:15:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
  
 import pandas as pd
+import streamlit
 
 streamlit.title("Breakfast")
 streamlit.text('🥣omega 3 & blueberry oatmeal')
@@ -8,7 +9,7 @@ streamlit.text('🍳hardboiled free range eggs')
 streamlit.text('🥑avocado toast')
 streamlit.header('🍓🫐build your own fruit smoothie🍌🍒')
 
-import streamlit
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('fruit')
 

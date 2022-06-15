eca51c2ce4534d97463a0cb79482410f7dc2da8e
fosternilsson516 <99349623+fosternilsson516@users.noreply.github.com>
2022-06-15 14:21:02 -0500

    Update streamlit_app.py
    added pick list #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ fruits_selected = streamlit.multiselect("pick some fruits:" , list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #displays data on page
-streamlit.dataframe(my_fruit_list) 
+streamlit.dataframe(fruits_to_show) 
 
